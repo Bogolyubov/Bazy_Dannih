da2ec2c46b033e633d93ee87ae75706bcf8dc192
Bogolyubov <79753802+Bogolyubov@users.noreply.github.com>
2021-12-21 20:54:53 +0500

    Add files via upload

---
+++ bd auto/save_edit.nalichie.php
@@ -0,0 +1,17 @@
+<html> <body>
+<?php
+
+$db=mysqli_connect("localhost", "root","root","cars") or die ("Невозможно
+подключиться к серверу");
+$zapros="UPDATE nalichie SET id_auto='".$_GET['id_auto'].
+"', id_salon='".$_GET['id_salon']."',stoim_auto='".$_GET['stoim_auto']."' WHERE id_nalichie="
+.$_GET['id'];
+
+mysqli_query($db,$zapros);
+if (mysqli_affected_rows($db)>0) {
+echo 'Все сохранено. <a href="index2.php"> Вернуться к списку
+автомобилей в наличии </a>'; }
+else { echo 'Ошибка сохранения, введите корректные данные. <a href="index2.php">
+Вернуться к списку автомобилей в наличии</a> '; }
+?>
+</body> </html>
---
+++ bd auto/vspom_nalichie.php
@@ -0,0 +1,29 @@
+<?php
+
+$db=mysqli_connect("localhost", "root","root","cars") or die ("Невозможно подключиться к серверу");
+mysqli_set_charset($db, "utf8");
+$query="SELECT marka_auto, model_auto,
+year_auto, trans_auto, vipusk_auto, stoim_auto FROM car WHERE id_auto=".$_GET['id_auto'];
+$result=mysqli_query($db,$query);
+while ($st = mysqli_fetch_assoc($result)) {
+$id=$_GET['id'];
+$marka= $st['marka_auto'];
+$model= $st['model_auto'];
+$year = (int) $st['year_auto'];
+$trans= $st['trans_auto'];
+$vipusk= $st['vipusk_auto'];
+}
+
+
+$query1="SELECT id_salon, name_salon,
+adres_salon FROM salon WHERE id_salon=".$_GET['id_salon'];
+$result1=mysqli_query($db,$query1);
+while ($st = mysqli_fetch_assoc($result1)) {
+$id=$_GET['id_salon'];
+$name= $st['name_salon'];
+$adres= $st['adres_salon'];
+}
+$stoim=$_GET['stoim_auto'];
+
+ ?>
+<?require_once 'save_new.nalichie.php';?>
